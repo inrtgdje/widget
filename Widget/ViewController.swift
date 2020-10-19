582274bd49b9212364c00605254089d4c95aea08
inrtgdje <ttm0402@163.com>
2020-10-19 12:10:21 +0800

    Initial Commit

---
+++ Widget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Widget
+//
+//  Created by 汤天明 on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
